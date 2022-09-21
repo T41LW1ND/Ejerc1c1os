9414edfadfca768a72f925e8a63e0cdcf1103b1d
sarade.andres <sarade.andres@educa.madrid.org>
2022-09-21 19:18:16 +0200

    Commit and push

---
+++ src/Ejercicio15.java
@@ -0,0 +1,8 @@
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 1;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
---
+++ src/Ejercicio16.java
@@ -0,0 +1,6 @@
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        int variable = 12345;
+        System.out.println(variable);
+    }
+}
